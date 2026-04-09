3c75453ed9b6441681383b9a3afd4e2db5a535bb
SurajShetty62 <surajshetty180@gmail.com>
2026-04-09 10:53:23 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -102,5 +102,5 @@ def download_report(filename):
 
 if __name__ == "__main__":
     import os
-port = int(os.environ.get("PORT", 5000))
-app.run(host="0.0.0.0", port=port, debug=False)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
